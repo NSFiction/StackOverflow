fea22a3ee1bf5750e620134c58503c653e7b82ff
brunov0id <bruno@belab.com.br>
2016-03-31 12:44:02 -0300

    Initial Commit

---
+++ StackOverflow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackOverflow
+//
+//  Created by Bruno da Luz on 3/31/16.
+//  Copyright © 2016 Bruno da Luz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
