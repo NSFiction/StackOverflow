e7b69676c3c4c2abb756202ccf624f06ac059496
brunodlz <brunolabx@gmail.com>
2016-06-20 22:23:06 -0300

    added protocol

---
+++ StackOverflow/StoryboardIdentifiable.swift
@@ -0,0 +1,13 @@
+//
+//  StoryboardIdentifiable.swift
+//  StackOverflow
+//
+//  Created by Bruno Da luz on 6/20/16.
+//  Copyright © 2016 Bruno da Luz. All rights reserved.
+//
+
+import UIKit
+
+protocol StoryboardIdentifiable {
+    static var storyboardIdentifier: String { get }
+}
