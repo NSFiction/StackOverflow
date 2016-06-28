276d763c6d11d03bbc98f59e1719af211a6c190d
brunodlz <brunolabx@gmail.com>
2016-06-28 00:38:22 -0300

    add icons

---
+++ StackOverflow/TopCell.swift
@@ -0,0 +1,25 @@
+//
+//  asasdTableViewCell.swift
+//  StackOverflow
+//
+//  Created by Bruno da Luz on 6/26/16.
+//  Copyright © 2016 nFiction. All rights reserved.
+//
+
+import UIKit
+
+class asasdTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var asdasd: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
