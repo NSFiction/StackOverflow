98500df7c0c02dd5461cb03fd794bc982d58851f
Bruno da Luz <brunolabx@gmail.com>
2017-05-27 02:13:30 -0300

    removed storyboard and searchBar

--- StackOverflow/Classes/Modules/Tag/TagViewController.swift
+++ StackOverflow/Classes/Modules/Tag/TagViewController.swift
@@ -3,35 +3,65 @@ import PKHUD
 
 class TagViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    var tableView: UITableView!
-    
+    fileprivate var tableView: UITableView!
+    private var categories = Array<String>()
+
     struct Cell {
         static let reuseIdentifier = "cellIdentifier"
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.navigationItem.title = "TAG"
         self.view.backgroundColor = .white
-        
+
         createLayout()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        loadCategories()
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: Cell.reuseIdentifier)
-        cell.textLabel?.text = "Test"
+
+        cell.textLabel?.text = categories[indexPath.item]
+
         return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+
+    private func loadCategories() {
+        if Network.hasConnection {
+            HUD.flash(.labeledProgress(title: nil, subtitle: "Please wait..."), delay: 60.0)
+
+            let consume = ConsumeTag()
+            consume.fetch({ (result) in
+
+                HUD.hide(animated: true)
+
+                switch result {
+                case .success(let value):
+                    self.categories = value
+                    self.tableView.reloadData()
+
+                case .failure(let error):
+                    print("An error occurred: \(error.localizedDescription)")
+                }
+            })
+            
+        }
+    }
 }
 
 extension TagViewController {
@@ -49,5 +79,5 @@ extension TagViewController {
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
-
+    
 }
