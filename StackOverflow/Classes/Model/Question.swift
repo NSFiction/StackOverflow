5494c71f94e3fd566a28c24e8eb3c05064d8522e
Bruno da Luz <brunolabx@gmail.com>
2017-09-28 00:39:03 -0300

    Create question class

---
+++ StackOverflow/Classes/Model/Question.swift
@@ -0,0 +1,11 @@
+//
+//  Question.swift
+//  StackOverflow
+//
+//  Created by Bruno da Luz on 9/28/17.
+//  Copyright © 2017 nFiction. All rights reserved.
+//
+
+protocol AnyQuestion: class {
+    
+}
